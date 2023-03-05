26faaaf4278ce7424545ea5e31bee6a1c1c31f9c
martindylan <dylanmartin1922@gmail.com>
2023-03-05 19:52:37 -0300

    cleaned code, implemented checkbox type and replaced input div for a textarea element

--- src/Components/Body/Note/NoteContent/Item/Item.tsx
+++ src/Components/Body/Note/NoteContent/Item/Item.tsx
@@ -1,5 +1,4 @@
 import { useState, FunctionComponent, useEffect, useRef, useCallback } from 'react';
-import { useGlobal } from '../../../../../GlobalContext';
 import { useNote } from '../../NoteContext';
 import styles from './Item.module.css';
 // Components
@@ -7,26 +6,23 @@ import Tools from './Tools/Tools';
 
 interface IItemProps {
   id: number;
-  removeItem: any;
+  removeItem: (id: number) => void;
   type: string;
-  addItem: (type: string) => void;
+  addItem: (type: string | null, pos: number | null) => void;
+  focus: boolean;
 }
 
 const Item: FunctionComponent<IItemProps> = (props) => {
   const [toolVisibility, setToolVisibility] = useState('hidden');
   const { note, setNote } = useNote();
-  const { id } = props;
-  const { global, setGlobal } = useGlobal();
+  const { id, type } = props;
+  const { checked } = note.items[id];
 
   const mainRef = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLSpanElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const checkboxRef = useRef<HTMLInputElement>(null);
 
-  // Set global current item to this item
-  const setToCurrent = (e: any) => {
-    setGlobal({ ...global, currentItem: id });
-    showTool(e);
-  }
-  // Show and hide item tools when hovering/focusing
+  // Show and hide item tool
   const showTool = (e: any) => {
     if (!e.currentTarget.contains(e.relatedTarget)) {
       setToolVisibility('visible');
@@ -38,21 +34,95 @@ const Item: FunctionComponent<IItemProps> = (props) => {
     }
   }
 
+  // Set width and height of textarea
+  const resize = () => {
+    const input = inputRef.current;
+    if (input) {
+      input.style.width = input.value.length + 'ch';
+      input.style.height = 'auto';
+      input.style.height = `calc(${input.scrollHeight}px - 0.75ch`;
+    };
+  }
+
+  // Toggle checkbox in items object
+  const toggle = () => {
+    let newItems = [...note.items];
+    newItems[id].checked = !checked;
+    setNote({ ...note, items: newItems })
+  }
+
+  // Focus item and set caret to end of content
+  const focusEnd = (element: any) => {
+    // Set caret to end
+    // const sel = window.getSelection();
+    // sel?.setBaseAndExtent(parent, 1, parent.childNodes[0], 0);
+    const input = element;
+    var len = input.value.length;
+
+    // Mostly for Web Browsers
+    if (input.setSelectionRange) {
+      input.focus();
+      input.setSelectionRange(len, len);
+    } else if (input.createTextRange) {
+      var t = input.createTextRange();
+      t.collapse(true);
+      t.moveEnd('character', len);
+      t.moveStart('character', len);
+      t.select();
+    }
+  }
+
+  // Set note focus to this item
+  // Show item tool
+  const setToCurrent = useCallback((e: any) => {
+    setNote((note: any) => ({ ...note, focus: id }));
+    showTool(e);
+  }, [setNote, id]);
+
+  // Updates the items object according to input's inner text
+  const updateItems = useCallback((e: any) => {
+    let newItems = [...note.items];
+    newItems[id].content = e.target.value;
+    setNote({ ...note, items: newItems });
+    resize();
+  }, [note, id, setNote]);
+
+  // Manage key presses
+  const keyDown = useCallback((e: any) => {
+    const input = inputRef.current;
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const newType = type === 'heading' ? 'text' : type;
+      props.addItem(newType, id + 1);
+    } else if (e.key === 'Backspace' && input?.value === '' && id !== 0) {
+      props.removeItem(id);
+    }
+  }, [props, id, type])
+
   // When mounting the component:
-  // Set main's event listeners for hover and focus
-  // Set input's inner text according to the items object
   useEffect(() => {
+    // Add main's event listeners for hover and focus
     const main = mainRef.current;
     main?.addEventListener('mouseenter', showTool);
     main?.addEventListener('focusin', setToCurrent);
     main?.addEventListener('mouseleave', hideTool);
     main?.addEventListener('focusout', hideTool);
+    // Focus item if it was previously focused for the given note
     const input = inputRef.current;
+    if (input && props.focus) {
+      focusEnd(input);
+    }
+    // Set input's value according to the items object
     const text = note.items[id].content;
-    if (input && text) {
-      input.innerText = text;
+    if (input) {
+      input.value = text ? text : '';
+    }
+    resize();
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = checked;
     }
     return () => {
+      // Remove main's event listeners for hover and focus
       main?.removeEventListener('mouseenter', showTool);
       main?.removeEventListener('focusin', showTool);
       main?.removeEventListener('mouseleave', hideTool);
@@ -60,53 +130,58 @@ const Item: FunctionComponent<IItemProps> = (props) => {
     }
   }, [])
 
-  // Set input's inner text when an item in the items array gets added or deleted
-  useEffect(() => {
-    const input = inputRef.current;
-    const text = note.items[id].content;
-    if (input && text) {
-      input.innerText = text;
-    }
-  }, [note.items.length])
-
-
-  // Updates the items object according to input's inner text
-  const updateItems = useCallback((e: any) => {
-    let newItems = [...note.items];
-    newItems[id].content = e.target.innerText;
-    setNote({ ...note, items: newItems });
-  }, [note, id, setNote]);
-
-  // Manage key presses
-  const keyPressed = useCallback((e: any) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      props.addItem('');
-    }
-  }, [props])
-
   // Set input's event listener for input and keypressed
   useEffect(() => {
     const input = inputRef.current;
     input?.addEventListener('input', updateItems);
-    input?.addEventListener('keypress', keyPressed);
+    input?.addEventListener('keydown', keyDown);
     return () => {
       input?.removeEventListener('input', updateItems);
-      input?.removeEventListener('keypress', keyPressed);
+      input?.removeEventListener('keydown', keyDown);
     }
-  }, [updateItems, keyPressed])
+  }, [updateItems, keyDown])
+
+  // Set input's inner text when an item in the items array gets added or deleted
+  useEffect(() => {
+    const input = inputRef.current;
+    if (input && props.focus) {
+      focusEnd(input);
+    }
+    const text = note.items[id].content;
+    if (input) {
+      input.value = text;
+    }
+  }, [note.items.length, props.focus])
 
   // Return JSX according to item type
   const makeItem = () => {
-    switch (props.type) {
+    switch (type) {
       case 'text':
-        return <span ref={inputRef} className={styles.text} contentEditable></span>;
-      case 'bulleted':
-        return <><div className={styles.bulleted}></div><span ref={inputRef} className={styles.text} contentEditable></span></>
+        return (
+          <textarea rows={1} ref={inputRef} className={styles.input} placeholder='...'></textarea>
+        );
       case 'heading':
-        return <span ref={inputRef} className={`${styles.text} ${styles.heading}`} contentEditable></span>;
+        return (
+          <textarea rows={1} ref={inputRef} className={`${styles.input} ${styles.heading}`} placeholder='...'></textarea>
+        );
+      case 'bulleted':
+        return (
+          <>
+            <div className={styles.bulleted}></div>
+            <textarea rows={1} ref={inputRef} className={styles.input} placeholder='...'></textarea>
+          </>
+        );
+      case 'checkbox':
+        return (
+          <>
+            <div className={`${styles.checkbox} checked${checked}`}><input ref={checkboxRef} type="checkbox" onClick={toggle}></input></div>
+            <textarea rows={1} ref={inputRef} className={`${styles.input} checked${checked}`} placeholder='...'></textarea>
+          </>
+        );
       default:
-        return <span ref={inputRef} className={styles.text} contentEditable></span>;
+        return (
+          <textarea ref={inputRef} className={styles.input} placeholder='...'></textarea>
+        );
     }
   }
 
