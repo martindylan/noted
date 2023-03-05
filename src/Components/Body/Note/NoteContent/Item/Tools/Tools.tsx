bce4596d26de2ee6726c0d096adfccfec95fbf38
martindylan <dylanmartin1922@gmail.com>
2023-03-05 19:54:25 -0300

    fixed img imports, began itemTypeMenu implementation

--- src/Components/Body/Note/NoteContent/Item/Tools/Tools.tsx
+++ src/Components/Body/Note/NoteContent/Item/Tools/Tools.tsx
@@ -1,9 +1,10 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import styles from './Tools.module.css';
 // Resources
-import trashcanImg from './img/trashcan.png';
-import changeImg from './img/change.png';
-import moveImg from './img/move.png'
+import trashcanImg from '../../../../../../Resources/Img/trashcan.png';
+import changeImg from '../../../../../../Resources/Img/change.png';
+import moveImg from '../../../../../../Resources/Img/move.png'
+import ItemTypeMenu from '../../../../../UI/ItemTypeMenu/ItemTypeMenu';
 
 type ToolsProps = {
   visibility: string;
@@ -13,15 +14,23 @@ type ToolsProps = {
 
 const Tools: FunctionComponent<ToolsProps> = (props) => {
 
+  const [displayChange, setdisplayChange] = useState(false);
+  const id = props.id;
+
   const remove = () => {
     const confirmation = window.confirm('Are you sure you want to delete this item?');
-    if (confirmation) props.removeItem(props.id);
+    if (confirmation) props.removeItem(id);
+  }
+
+  const changeType = (type: string | null) => {
+    
   }
 
   return (
     <div className={`${styles.this} ${styles[props.visibility]}`}>
       <button className={styles.clickable} onClick={remove}><img className={styles.first} draggable="false" src={trashcanImg} alt="delete"></img></button>
-      <button className={styles.clickable} onClick={(e) => console.log("change item")}><img className={styles.second} draggable="false" src={changeImg} alt="change"></img></button>
+      {displayChange && <ItemTypeMenu sendTypeToParent={changeType} id={id} />}
+      <button className={styles.clickable} onClick={(e) => setdisplayChange(true)}><img className={styles.second} draggable="false" src={changeImg} alt="change"></img></button>
       <button className={styles.draggable} onClick={(e) => console.log("move item")}><img className={styles.last} draggable="false" src={moveImg} alt="move"></img></button>
     </div>
   )
