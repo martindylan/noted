c45ff178f2bb2c972ad1b2ea09b78a90529ceb6a
martindylan <dylanmartin1922@gmail.com>
2023-03-06 19:30:45 -0300

    fixed focus issues and implemented type changing

--- src/Components/Body/Note/NoteContent/Item/Tools/Tools.tsx
+++ src/Components/Body/Note/NoteContent/Item/Tools/Tools.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, useState } from 'react';
 import styles from './Tools.module.css';
+import button from '../../../../../../Resources/CSS/button.module.css';
 // Resources
 import trashcanImg from '../../../../../../Resources/Img/trashcan.png';
 import changeImg from '../../../../../../Resources/Img/change.png';
@@ -10,11 +11,12 @@ type ToolsProps = {
   visibility: string;
   id: number;
   removeItem: (id: number) => void;
+  changeType: (newType: string) => void;
 }
 
 const Tools: FunctionComponent<ToolsProps> = (props) => {
 
-  const [displayChange, setdisplayChange] = useState(false);
+  const [displayChange, setDisplayChange] = useState(false);
   const id = props.id;
 
   const remove = () => {
@@ -22,16 +24,24 @@ const Tools: FunctionComponent<ToolsProps> = (props) => {
     if (confirmation) props.removeItem(id);
   }
 
-  const changeType = (type: string | null) => {
-    
+  const getType = (type: string) => {
+    props.changeType(type);
+  }
+
+  const focusOut = (e: any) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setDisplayChange(false);
+    }
   }
 
   return (
     <div className={`${styles.this} ${styles[props.visibility]}`}>
-      <button className={styles.clickable} onClick={remove}><img className={styles.first} draggable="false" src={trashcanImg} alt="delete"></img></button>
-      {displayChange && <ItemTypeMenu sendTypeToParent={changeType} id={id} />}
-      <button className={styles.clickable} onClick={(e) => setdisplayChange(true)}><img className={styles.second} draggable="false" src={changeImg} alt="change"></img></button>
-      <button className={styles.draggable} onClick={(e) => console.log("move item")}><img className={styles.last} draggable="false" src={moveImg} alt="move"></img></button>
+      <button className={`${styles.clickable} ${button.button}`} onClick={remove}><img className={styles.first} draggable="false" src={trashcanImg} alt="delete"></img></button>
+      <div className={styles.changeType} onBlur={focusOut}>
+        <button className={`${styles.clickable} ${button.button}`} onClick={(e) => setDisplayChange(true)}><img className={styles.second} draggable="false" src={changeImg} alt="change"></img></button>
+        <ItemTypeMenu visibility={displayChange} sendTypeToParent={getType} id={id} />
+      </div>
+      <button className={`${styles.draggable} ${button.button}`} onClick={(e) => console.log("move item")}><img className={styles.last} draggable="false" src={moveImg} alt="move"></img></button>
     </div>
   )
 }
