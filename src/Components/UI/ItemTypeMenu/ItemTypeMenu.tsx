39c74f09cf34bcbcaffaec0b2f7ac7c9e65372d7
martindylan <dylanmartin1922@gmail.com>
2023-03-06 19:25:29 -0300

    fixed changing type logic and styling

--- src/Components/UI/ItemTypeMenu/ItemTypeMenu.tsx
+++ src/Components/UI/ItemTypeMenu/ItemTypeMenu.tsx
@@ -1,31 +1,52 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import styles from './ItemTypeMenu.module.css';
 import text from './img/text.png';
 import heading from './img/heading.png';
 import bulleted from './img/bulleted.png';
 import checkbox from './img/checkbox.png';
+import { useNote } from '../../Body/Note/NoteContext';
 
 interface IItemTypeMenu {
-  sendTypeToParent: (type: string | null, pos: number | null) => void;
+  sendTypeToParent: (type: string, pos: number | null) => void;
   id: number | null;
+  visibility: boolean;
 }
 
 const ItemTypeMenu: FunctionComponent<IItemTypeMenu> = (props) => {
 
-  const sendTypeToParent = props.sendTypeToParent;
+  const { note } = useNote();
   const id = props.id;
+  const [type, setType] = useState(id !== null ? note.items[id].type : null);
+  const sendType = props.sendTypeToParent;
+
+  useEffect(() => {
+    setType(id !== null ? note.items[id].type : null);
+  }, [id !== null ? note.items[id].type : null])
+
+  const isText = id !== null && type === 'text';
+  const isHeading = id !== null && type === 'heading';
+  const isBulleted = id !== null && type === 'bulleted';
+  const isCheckbox = id !== null && type === 'checkbox';
 
   return (
-    <div className={styles.this}>
+    <div className={styles.this} style={{ visibility: props.visibility ? 'visible' : 'hidden', opacity: props.visibility ? 1 : 0 }}>
       <div className={styles.row}>
-        <button onClick={() => sendTypeToParent('text', id ? id : null)}><img src={text} alt='text' draggable="false" /></button>
-        <button onClick={() => sendTypeToParent('heading', id ? id : null)}><img src={heading} alt='heading' draggable="false" /></button>
+        <button className={isText ? styles.disabled : styles.enabled} disabled={isText} onClick={() => sendType('text', id ? id : null)}>
+          <img src={text} alt='text' draggable="false" />
+        </button>
+        <button className={isHeading ? styles.disabled : styles.enabled} disabled={isHeading} onClick={() => sendType('heading', id ? id : null)}>
+          <img src={heading} alt='heading' draggable="false" />
+        </button>
       </div>
       <div className={styles.row}>
-        <button onClick={() => sendTypeToParent('bulleted', id ? id : null)}><img src={bulleted} alt='bulleted' draggable="false" /></button>
-        <button onClick={() => sendTypeToParent('checkbox', id ? id : null)}><img src={checkbox} alt='checkbox' draggable="false" /></button>
+        <button className={isBulleted ? styles.disabled : styles.enabled} disabled={isBulleted} onClick={() => sendType('bulleted', id ? id : null)}>
+          <img src={bulleted} alt='bulleted' draggable="false" />
+        </button>
+        <button className={isCheckbox ? styles.disabled : styles.enabled} disabled={isCheckbox} onClick={() => sendType('checkbox', id ? id : null)}>
+          <img src={checkbox} alt='checkbox' draggable="false" />
+        </button>
       </div>
-    </div>
+    </div >
   )
 }
 export default ItemTypeMenu;
