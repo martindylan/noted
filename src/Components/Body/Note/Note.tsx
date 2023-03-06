04d03e1da2144c12e837be6b9a1e6c1b99e907cc
martindylan <dylanmartin1922@gmail.com>
2023-03-06 19:27:14 -0300

    implemented scrolling

--- src/Components/Body/Note/Note.tsx
+++ src/Components/Body/Note/Note.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'react';
 import styles from './Note.module.css';
+import scrollable from '../../../Resources/CSS/scrollable.module.css';
 // Components
 import { NoteContextProvider } from './NoteContext';
 import NoteTitle from './NoteTitle/NoteTitle';
@@ -11,10 +12,12 @@ interface INoteProps {
 
 const Note: FunctionComponent<INoteProps> = (props) => {
   return (
-    <div className={styles.this}>
+    <div className={`${styles.this}`} >
       <NoteContextProvider note={props.note}>
-        <NoteTitle></NoteTitle>
-        <NoteContent></NoteContent>
+        <div className={scrollable.scrollable}>
+          <NoteTitle></NoteTitle>
+          <NoteContent></NoteContent>
+        </div>
       </NoteContextProvider>
     </div>
   )
