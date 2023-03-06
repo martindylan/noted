2bac9b84e5a035100050ebbda86f38ae8f40b631
martindylan <dylanmartin1922@gmail.com>
2023-03-05 21:48:37 -0300

    removed wrapping divs around components for a cleaner css

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -7,17 +7,12 @@ import Note from './Note/Note';
 export default function Body() {
 
   const { global, } = useGlobal();
-  const notes = global.notes;
   const currentNote = global.currentNote;
 
   return (
     <div className={styles.this}>
-      <div className={styles.nav}>
-        <Nav></Nav>
-      </div>
-      <div className={styles.noteParent}>
-        <Note key={currentNote} note={currentNote}></Note>
-      </div>
+      <Nav></Nav>
+      <Note key={currentNote} note={currentNote}></Note>
     </div>
   )
 }
