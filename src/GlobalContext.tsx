bec3427980a2093eb1d7e0daeb07a6c2ced83d08
martindylan <dylanmartin1922@gmail.com>
2023-03-06 19:12:13 -0300

    documented code

--- src/GlobalContext.tsx
+++ src/GlobalContext.tsx
@@ -11,8 +11,8 @@ interface IGlobalProviderProps {
 }
 export const GlobalProvider: FunctionComponent<IGlobalProviderProps> = ({ children }) => {
   const storedData = window.localStorage.getItem('data');
+  // If there's no data saved to localStorage, use the default data.
   const [global, setGlobal] = useState(storedData ? JSON.parse(storedData) : defaultData);
-
   localStorage.setItem('data', JSON.stringify(global));
 
   return (
@@ -22,25 +22,25 @@ export const GlobalProvider: FunctionComponent<IGlobalProviderProps> = ({ childr
   )
 }
 
+// This is the structure of the data used by the app, with defaults in case the app wasn't used yet.
 const defaultData = {
-  lang: 'EN',
-  currentNote: 0,
+  theme: 'dark',  // UI theme (dark/light)
+  currentNote: 0, // Index of the note that's being edited
   notes:
     [
       {
-        title: `I'm your first note...`,
-        focus: 1,
-        items:
+        title: `I'm your first note...`, // Note title
+        focus: 1, // Index of the item that's being edited
+        items:  // Items inside note
         [
           {
-            content: `...and we're items!`,
-            type: 'text',
-            checked: false
+            content: `...and we're items!`, // Item text
+            type: 'text' // Item type (text/heading/bulleted/checkbox)
+            // checked: false // For checkbox type items only, specifies if it's checked or not
           },
           {
             content: `We can be changed, edited, moved or deleted.`,
-            type: 'text',
-            checked: false
+            type: 'text'
           }
         ]
       }
